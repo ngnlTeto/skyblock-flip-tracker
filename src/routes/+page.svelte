feb951dd01df0b75bf14cf85b388f6567e33965d
Henry <henry@gedert.de>
2026-05-10 22:55:16 +0200

    😭

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
 				notes: flip.notes,
 				profit:
 					outputItem.sellPrice !== null && outputItem.sellPrice !== undefined
-						? outputItem.sellPrice - sum(inputItems.map((i) => i.buyPrice ?? 0))
+						? outputItem.sellPrice - sum(inputItems.map((i) => i.buyPrice ?? 0 * i.quantity))
 						: null,
 				totalInputPrice
 			};
